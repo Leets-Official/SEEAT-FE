690caaae842fca3f751a2d640014f141ae9388c5
Dahyeon <dahhyeon22@gmail.com>
2025-06-29 19:54:22 +0900

    feat: 라우팅 설정 및 주요 페이지 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import router from '@/routes/route';
+
 function App() {
-  return (
-    <div className="App">
-      <div className="text-3xl font-bold text-blue-500 underline">Hello World!!</div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;

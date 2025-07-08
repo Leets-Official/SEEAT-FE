4c1d889580b7383c6b23a16cc28acc32a61bb6f1
Dahyeon <dahhyeon22@gmail.com>
2025-07-08 23:40:33 +0900

    chore: 전역 테마, 유틸리티 정의

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,4 +1,4 @@
 const Signup = () => {
-  return <div className="p-4 text-lg">회원가입 페이지012334990</div>;
+  return <div className="text-title-3 p-4 text-lg text-red-100">회원가입 페이지012334990</div>;
 };
 export default Signup;

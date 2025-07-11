8fc67ada4428d12fbe9aa42090adfc8ff9a28907
Dahyeon <dahhyeon22@gmail.com>
2025-07-12 01:13:42 +0900

    feat: 뱃지 컴포넌트 구현

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,4 +1,12 @@
+import Badge from '@/components/common/Badge/Badge';
+
 const Signup = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Badge type="tag">태그</Badge>
+      <Badge type="info">후기 정보</Badge>
+      <Badge type="removable">Text</Badge>
+    </div>
+  );
 };
 export default Signup;

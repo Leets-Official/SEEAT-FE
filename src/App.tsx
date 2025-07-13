beb6d02c2c93854eda1640443ebd848b1c943ad2
Saelyeam <eileen4505@gachon.ac.kr>
2025-07-13 23:53:13 +0900

    App.tsx 및 global.css index.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-//App.tsx
-import { RouterProvider } from 'react-router-dom';
-import router from '@/routes/route';
-
 function App() {
-  return <RouterProvider router={router} />;
+
 }
 
 export default App;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 import Button from '@/components/common/Button';
+import ToggleTab from './common/ToggleTab';
 
 export { Button };
+export { ToggleTab };

4298ce0d87f9efa7e92f3681998e4d809b54e5de
Saelyeam <eileen4505@gachon.ac.kr>
2025-07-13 23:46:10 +0900

    feat: ToggleTab 컴포넌트를 Tailwind 기반으로 마이그레이션

--- src/components/common/ToggleTab/ToggleTab.tsx
+++ src/components/common/ToggleTab/ToggleTab.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import type{ ToggleTabProps } from './ToggleTab.types';
-import { Wrapper, TabGroup, TabButton, Slider } from './ToggleTab.styled';
+import type { ToggleTabProps } from './ToggleTab.types';
 
 const ToggleTab = ({ options, selected, onSelect }: ToggleTabProps) => {
   const selectedIndex = options.findIndex((option) => option === selected);
 
   return (
-    <Wrapper>
-      <TabGroup>
-        <Slider index={selectedIndex} />
+    <div className="relative flex flex-col gap-5 p-4 bg-transparent w-full">
+      <div className="relative flex w-full max-w-[335px] h-12 bg-[rgba(66,66,66,0.3)] rounded-[12px] px-1 py-1 gap-[10px]">
+        {/* Slider */}
+        <div
+          className="absolute top-2 h-[calc(100%-16px)] w-[calc(50%-14px)] border-[1.5px] border-gray-500 rounded-[8px] transition-all duration-300 z-10"
+          style={{
+            left: `calc(${selectedIndex} * 50% + 7px)`,
+          }}
+        />
+
+        {/* Buttons */}
         {options.map((option) => (
-          <TabButton
+          <button
             key={option}
-            selected={option === selected}
             onClick={() => onSelect(option)}
+            className={`flex-1 z-20 text-[16px] font-normal py-[10px] transition-colors duration-200 whitespace-nowrap overflow-hidden text-ellipsis ${
+              option === selected ? 'text-white' : 'text-white/30'
+            }`}
           >
             {option}
-          </TabButton>
+          </button>
         ))}
-      </TabGroup>
-    </Wrapper>
+      </div>
+    </div>
   );
 };
 
--- src/components/common/ToggleTab/index.ts
+++ src/components/common/ToggleTab/index.ts
@@ -1,1 +1,2 @@
 export { default } from './ToggleTab';
+export * from './ToggleTab.types';

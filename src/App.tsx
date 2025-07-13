7a941144fcb58249a613a50dd54094258ab38cb0
Saelyeam <eileen4505@gachon.ac.kr>
2025-07-14 00:18:59 +0900

    fix: 빌드에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import router from '@/routes/route';
+
 const App = () => {
-  return (
-    <div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;

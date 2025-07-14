cc930fc687c46a15fcb040a92731d2e25c8561fe
Dahyeon <dahhyeon22@gmail.com>
2025-07-14 16:32:48 +0900

    chore: 불필요한 코드 삭제

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,15 +1,4 @@
-import Badge from '@/components/common/Badge/Badge';
-import BadgeExample from '@/components/examples/BadgeExamples';
-import ButtonExamples from '@/components/examples/ButtonExamples';
-import ImageExamples from '@/components/examples/ImageExamples';
-
 const Signup = () => {
-  return (
-    <div>
-      <ButtonExamples />
-      <BadgeExample />
-      <ImageExamples />
-    </div>
-  );
+  return <div></div>;
 };
 export default Signup;

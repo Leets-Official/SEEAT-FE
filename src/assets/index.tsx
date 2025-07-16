b4e27d5a659f1f88e8ad09e9062b828abf96e5be
soyun-git121 <soyunbag066@gmail.com>
2025-07-16 21:05:59 +0900

    🔥 fix : 충돌 수정

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,12 +1,20 @@
-import CloseIcon from './icons/close.svg?react';
-import ChevronIcon from './icons/chevron.svg?react';
-import HeartFilledIcon from './icons/heart_f.svg?react';
-import HeartOutlineIcon from './icons/heart_o.svg?react';
-import BookmarkFilledIcon from './icons/bookmark_f.svg?react';
-import BookmarkOutlineIcon from './icons/bookmark_o.svg?react';
-import PlusIcon from './icons/plus.svg?react';
+import HeartOn from '@/assets/icons/heart_on.svg?react';
+import HomeIcon from '@/assets/icons/home.svg?react';
+import SearchIcon from '@/assets/icons/search.svg?react';
+import MyIcon from '@/assets/icons/my.svg?react';
+import CloseIcon from '@/assets/icons/close.svg?react';
+import ChevronIcon from '@/assets/icons/chevron.svg?react';
+import HeartFilledIcon from '@/assets/icons/heart_f.svg?react';
+import HeartOutlineIcon from '@/assets/icons/heart_o.svg?react';
+import BookmarkFilledIcon from '@/assets/icons/bookmark_f.svg?react';
+import BookmarkOutlineIcon from '@/assets/icons/bookmark_o.svg?react';
+import PlusIcon from '@/assets/icons/plus.svg?react';
 
 export {
+  HeartOn,
+  HomeIcon,
+  SearchIcon,
+  MyIcon,
   CloseIcon,
   ChevronIcon,
   HeartFilledIcon,

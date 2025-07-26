e5195e6e4496e2dd2d59cd45edfb3f3114054741
Saelyeam <eileen4505@gachon.ac.kr>
2025-07-27 00:11:59 +0900

    style: ProgressBar 컴포넌트 스타일 수정

--- src/components/common/ProgressBar/ProgressBar.tsx
+++ src/components/common/ProgressBar/ProgressBar.tsx
@@ -7,18 +7,22 @@ const ProgressBar = ({ currentStep, totalSteps }: ProgressBarProps) => {
   const percentage = (currentStep / totalSteps) * 100;
 
   return (
-    <div className="px-6 mt-6 mb-2">
-      {/* 진행 바 */}
-      <div className="h-2 w-full bg-gray-800 relative">
-        <div
-          className="absolute left-0 top-0 h-full bg-red-400"
-          style={{ width: `${percentage}%` }}
-        />
+    <>
+      {/* 진행도 바 */}
+      <div className="mt-6 mb-2">
+        <div className="h-2 w-full bg-gray-800 relative">
+          <div
+            className="absolute left-0 top-0 h-full bg-red-400"
+            style={{ width: `${percentage}%` }}
+          />
+        </div>
       </div>
 
-      {/* 텍스트 */}
-      <div className="text-title-1 text-white mt-2">{`${currentStep}/${totalSteps}`}</div>
-    </div>
+      {/* 진행 텍스트 */}
+      <div className="text-title-1 text-white mb-3 px-6">
+        {`${currentStep}/${totalSteps}`}
+      </div>
+    </>
   );
 };
 

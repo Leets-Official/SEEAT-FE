126c6ddd81c33429cd1e8e6577e2584c15d13771
Dahyeon <dahhyeon22@gmail.com>
2025-07-27 20:57:12 +0900

    feat: #21 api 인스턴스 추가 및 요청인터셉터 추가

---
+++ src/api/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 7000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+// 요청 인터셉터
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default api;

83ca8a3b8ab59dcccb11d02c6cde82de48b635c0
soyun-git121 <soyunbag066@gmail.com>
2025-07-31 16:11:37 +0900

    🔥 fix : 디버깅

--- src/pages/my/CinemaChoice.tsx
+++ src/pages/my/CinemaChoice.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, HeaderBasic, ToggleTab } from '@/components';
 import { MoreVerticalIcon } from '@/assets';

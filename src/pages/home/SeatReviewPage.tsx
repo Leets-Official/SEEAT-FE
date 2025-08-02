f8c380d8b376d081bd18bbbb9a2f760c21200ecd
sispo3314 <sispo3314@gmail.com>
2025-08-03 00:21:08 +0900

    refactor: 중복으로 사용된 스타일 제거

--- src/pages/home/SeatReviewPage.tsx
+++ src/pages/home/SeatReviewPage.tsx
@@ -14,7 +14,7 @@ const SeatReviewPage = () => {
   const seatLabel = seatInfo?.seatNumber || '';
 
   return (
-    <div className="flex min-h-screen max-w-[430px] flex-col bg-gray-900 pt-11 pb-5">
+    <div className="flex min-h-screen flex-col pt-11 pb-5">
       <div className="fixed top-0 right-0 left-0 z-50 bg-gray-900">
         <Header onBackClick={() => navigate('')} />
       </div>

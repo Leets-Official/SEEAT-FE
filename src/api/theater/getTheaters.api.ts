dad3c9f18ebf968887e094bea37278e668fa65af
Dahyeon <dahhyeon22@gmail.com>
2025-08-03 22:15:41 +0900

    refactor: #35 api 인스턴스 응답/에러 타입을 활용하도록 수정

--- src/api/theater/getTheaters.api.ts
+++ src/api/theater/getTheaters.api.ts
@@ -1,5 +1,7 @@
 import api from '@/api/api';
+import type { ApiError, ApiResponse } from '@/types/api-response';
 import type { CinemaFormat } from '@/types/onboarding';
+import type { Theater } from '@/types/theater';
 
 export interface GetTheatersParams {
   type: CinemaFormat;
@@ -7,9 +9,13 @@ export interface GetTheatersParams {
   size: number;
 }
 
-export const getTheaters = async ({ type, page = 1, size = 10 }: GetTheatersParams) => {
+export const getTheaters = async ({
+  type,
+  page = 1,
+  size = 10,
+}: GetTheatersParams): Promise<Theater[]> => {
   try {
-    const res = await api.get('/theaters', {
+    const res = await api.get<ApiResponse<{ content: Theater[] }>>('/theaters', {
       params: {
         auditoriumType: type,
         page,
@@ -17,16 +23,18 @@ export const getTheaters = async ({ type, page = 1, size = 10 }: GetTheatersPara
       },
     });
 
-    const content = res.data.data.content;
+    const content = res.data.data?.content;
 
     if (!content) {
-      throw new Error('Theater content is missing in response');
+      throw new Error('영화관 목록에 응답에 없습니다.');
     }
 
     console.log('영화관 목록 응답:', content);
     return content;
-  } catch (e) {
-    console.error('영화관 목록 호출 실패:', e);
-    throw e;
+  } catch (error) {
+    // ApiError 타입 캐스팅
+    const apiError = error as ApiError;
+    console.error('영화관 목록 호출 실패:', apiError.message, apiError.error);
+    throw apiError;
   }
 };

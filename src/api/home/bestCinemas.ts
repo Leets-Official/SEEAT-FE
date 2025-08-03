22270b79c02c24bbbfaee4cc37afb9b012cb5e5c
sispo3314 <sispo3314@gmail.com>
2025-08-04 04:47:11 +0900

    refactor: 필요 없는 부분 삭제

--- src/api/home/bestCinemas.ts
+++ src/api/home/bestCinemas.ts
@@ -1,16 +1,8 @@
-import type { ApiResponse } from '@/types/api-response';
 import api from '../api';
 import type { BestCinema } from '@/types/bestCinema';
 
 export const getBestCinemas = async (): Promise<BestCinema[]> => {
-  const res = await api.get<
-    ApiResponse<{
-      content: BestCinema[];
-      hasNext: boolean;
-      page: number;
-      size: number;
-    }>
-  >('/api/v1/home/auditoriums', {
+  const res = await api.get('/api/v1/home/auditoriums', {
     params: { page: 1, size: 4 },
   });
 

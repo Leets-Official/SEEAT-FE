9b78d351bbe16df623e5cdb24b35e9fe7725fded
Dahyeon <dahhyeon22@gmail.com>
2025-08-05 01:16:33 +0900

    chore: #52 코드 복구

--- src/api/hashtag/hashtag.ts
+++ src/api/hashtag/hashtag.ts
@@ -3,6 +3,6 @@ import api from '../api';
 import type { Hashtag } from '@/types/hashtag';
 
 export const getHashtags = async (): Promise<Hashtag[]> => {
-  const res = await api.get<ApiResponse<Hashtag[]>>('/hashtag');
+  const res = await api.get<ApiResponse<Hashtag[]>>('/api/v1/hashtag');
   return res.data.data;
 };

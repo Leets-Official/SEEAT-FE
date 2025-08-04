4994d29d3e5ab8ac463382647a117afef31bc39a
sispo3314 <sispo3314@gmail.com>
2025-08-04 19:57:14 +0900

    style: 베스트 시네마 카드 평점 표시 수정

--- src/components/common/BestCinemaCard/BestCinemaCard.tsx
+++ src/components/common/BestCinemaCard/BestCinemaCard.tsx
@@ -48,7 +48,7 @@ export default function BestCinemaCard({
         <div className="flex items-center">
           <StarSmall className="h-4 w-4" />
           <span className="text-caption-3 ml-1 text-white">
-            {typeof rating === 'number' ? rating.toFixed(1) : '평점 없음'}
+            {typeof rating === 'number' ? rating.toFixed(1) : '0.0'}
           </span>
           <span className="text-caption-3 ml-3 text-gray-500">후기</span>
           <span className="text-caption-1 ml-1 text-red-300">{reviewCount}</span>

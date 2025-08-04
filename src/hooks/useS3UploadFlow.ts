3ad8d2299ac0af8b7a7aa40b64a20894a4106d8d
Dahyeon <dahhyeon22@gmail.com>
2025-08-05 01:14:52 +0900

    chore: #52 주석 코드 위치 변경

--- src/hooks/useS3UploadFlow.ts
+++ src/hooks/useS3UploadFlow.ts
@@ -1,3 +1,36 @@
+import { getPresignedUrls, uploadToS3 } from '@/api/image/image.api';
+
+export const useS3UploadFlow = async (
+  files: File[],
+): Promise<{ fileName: string; url: string }[]> => {
+  if (files.length === 0) return [];
+
+  const fileNames = files.map((file) => file.name);
+
+  const response = await getPresignedUrls(fileNames);
+  const presignedInfos = Array.isArray(response) ? response : [response];
+
+  if (!presignedInfos || presignedInfos.length !== files.length) {
+    throw new Error('Presigned URL을 일부 가져오지 못했습니다.');
+  }
+
+  const uploadResults = await Promise.all(
+    files.map(async (file) => {
+      const matched = presignedInfos.find((p) => p.fileName === file.name);
+      if (!matched) throw new Error(`${file.name}에 대한 presigned URL이 없습니다.`);
+
+      await uploadToS3(matched.preSignedUrl, file);
+
+      return {
+        fileName: file.name,
+        url: matched.preSignedUrl,
+      };
+    }),
+  );
+
+  return uploadResults;
+};
+
 // import { usePresignedUrlMutation } from '@/hooks/mutations/usePresignedUrlMutation';
 // import { useUploadToS3Mutation } from '@/hooks/mutations/useUploadToS3Mutation';
 
@@ -36,38 +69,3 @@
 
 //   return { uploadImagesToS3 };
 // };
-
-import { getPresignedUrls, uploadToS3 } from '@/api/image/image.api';
-
-export const useS3UploadFlow = async (
-  files: File[],
-): Promise<{ fileName: string; url: string }[]> => {
-  if (files.length === 0) return [];
-
-  const fileNames = files.map((file) => file.name);
-
-  const response = await getPresignedUrls(fileNames);
-  const presignedInfos = Array.isArray(response) ? response : [response];
-
-  console.log('presigned Info 응답 ', presignedInfos);
-
-  if (!presignedInfos || presignedInfos.length !== files.length) {
-    throw new Error('Presigned URL을 일부 가져오지 못했습니다.');
-  }
-
-  const uploadResults = await Promise.all(
-    files.map(async (file) => {
-      const matched = presignedInfos.find((p) => p.fileName === file.name);
-      if (!matched) throw new Error(`${file.name}에 대한 presigned URL이 없습니다.`);
-
-      await uploadToS3(matched.preSignedUrl, file);
-
-      return {
-        fileName: file.name,
-        url: matched.preSignedUrl,
-      };
-    }),
-  );
-
-  return uploadResults;
-};

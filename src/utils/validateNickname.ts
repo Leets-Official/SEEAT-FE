b512a5a903a97a23e53f0484ae407baa40086896
Dahyeon <dahhyeon22@gmail.com>
2025-08-07 03:44:26 +0900

    chore: #34 유효성 검사 메세지 수정

--- src/utils/validateNickname.ts
+++ src/utils/validateNickname.ts
@@ -9,7 +9,7 @@ export const validateNickname = (nickname: string): { valid: boolean; message?: 
     return { valid: false, message: '닉네임은 8자 이하로 입력해주세요.' };
   }
   if (!/^[가-힣a-zA-Z0-9]+$/.test(trimmed)) {
-    return { valid: false, message: '닉네임에는 특수문자는 사용할 수 없어요.' };
+    return { valid: false, message: '닉네임에 특수문자는 사용할 수 없어요.' };
   }
 
   return { valid: true };

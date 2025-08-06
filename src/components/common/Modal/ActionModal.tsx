b9cc332237d302154010127b3c04a8710d7376e6
Dahyeon <dahhyeon22@gmail.com>
2025-08-06 21:42:47 +0900

    refactor: #32 style 수정

--- src/components/common/Modal/ActionModal.tsx
+++ src/components/common/Modal/ActionModal.tsx
@@ -8,15 +8,15 @@ interface ActionModalProps {
 const ActionModal = ({ onEdit, onDelete }: ActionModalProps) => {
   return (
     <BaseModal>
-      <div className="rounded-m flex h-[84px] w-[100px] flex-col items-center justify-center gap-1 bg-gray-600 px-3 py-3 shadow-md">
+      <div className="rounded-m flex h-[84px] w-[100px] flex-col items-stretch justify-center gap-1 bg-gray-600 px-2 py-3 shadow-md">
         <button
-          className="text-body-2 w-full rounded-s py-2 hover:bg-gray-800 active:bg-gray-900"
+          className="text-body-2 w-full rounded-s py-1.5 hover:bg-gray-800 active:bg-gray-900"
           onClick={onEdit}
         >
           수정하기
         </button>
         <button
-          className="text-body-2 w-full rounded-s py-2 hover:bg-gray-800 active:bg-gray-900"
+          className="text-body-2 w-full rounded-s py-1.5 hover:bg-gray-800 active:bg-gray-900"
           onClick={onDelete}
         >
           삭제하기

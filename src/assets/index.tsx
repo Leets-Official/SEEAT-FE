9ea73ad741e1d0a5e47294b220eef622fe251231
soyun-git121 <soyunbag066@gmail.com>
2025-08-06 20:51:39 +0900

    임시 수정 커밋

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -39,7 +39,6 @@ import NaverIcon from '@/assets/icons/naver_icon.svg?react';
 import SoundIcon from '@/assets/icons/sound.svg?react';
 import EnvironmentIcon from '@/assets/icons/environment.svg?react';
 import CompanionIcon from '@/assets/icons/companion.svg?react';
-
 import TicketAlt from '@/assets/gif/ticket_alt.gif';
 
 export {

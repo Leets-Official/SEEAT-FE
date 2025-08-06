9362e654a38ecacbefbff1c7dd21be029c59aba5
Dahyeon <dahhyeon22@gmail.com>
2025-08-06 21:51:11 +0900

    style: #32 base modal에서 액션모달 위치 조정

--- src/components/common/Modal/BaseModal.tsx
+++ src/components/common/Modal/BaseModal.tsx
@@ -14,7 +14,7 @@ const BaseModal = ({ children }: BaseModalProps) => {
     return (
       <>
         <div className="fixed inset-0 z-40 bg-gray-800/20" onClick={closeModal} />
-        <div className="absolute top-[22px] right-[-2px] z-50" onClick={(e) => e.stopPropagation()}>
+        <div className="absolute top-[22px] right-1 z-50" onClick={(e) => e.stopPropagation()}>
           {children}
         </div>
       </>

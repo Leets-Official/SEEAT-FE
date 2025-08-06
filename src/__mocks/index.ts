751bc320e5e2d85ebf3d1a54fa9dcd483da93e2e
sispo3314 <sispo3314@gmail.com>
2025-08-06 21:20:27 +0900

    fix: 빌드 오류 수정

--- src/__mocks/index.ts
+++ src/__mocks/index.ts
@@ -1,15 +1,7 @@
-import { bestCinemas } from './bestCinemaData';
 import { getRandomImage } from './mockImage';
 import { reviewDetailMock } from './reviewDetailMock';
 import { reviewSummaryMock } from './reviewSummaryMock';
 import { bestReviewMock } from './bestReviewMock';
 import { seatReviewMock } from './seatReviewMock';
 
-export {
-  bestCinemas,
-  getRandomImage,
-  reviewSummaryMock,
-  reviewDetailMock,
-  bestReviewMock,
-  seatReviewMock,
-};
+export { getRandomImage, reviewSummaryMock, reviewDetailMock, bestReviewMock, seatReviewMock };

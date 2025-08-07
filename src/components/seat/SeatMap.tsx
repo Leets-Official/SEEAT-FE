e2b949a52d5c9c9564d6976dffdd24c141616190
Dahyeon <dahhyeon22@gmail.com>
2025-08-07 16:33:54 +0900

    refactor: #92 좌석 배치도 아이템 수정

--- src/components/seat/SeatMap.tsx
+++ src/components/seat/SeatMap.tsx
@@ -6,8 +6,8 @@ import { useRef } from 'react';
 interface SeatMapProps {
   auditoriumId?: string;
   onSeatClick?: (seatId: string) => void;
-  focusedSeatIds?: string[]; // seatFocus용
-  selectedSeatNames?: string[]; // seatWrite 용
+  focusedSeatIds?: string[];
+  selectedSeatNames?: string[];
   seatData?: SeatRatingInfo[];
   type?: 'seatFocus' | 'seatPicker' | 'seatWrite';
 }
@@ -21,40 +21,34 @@ const SeatMap = ({
 }: SeatMapProps) => {
   const seatRows: Record<string, Seat[]> = {};
   const focusedRef = useRef<HTMLDivElement>(null!);
+  const allColumns = new Set<number>();
 
-  // row별로 묶기
+  // row별로 묶기 + 전체 column 수 수집
   seatData.forEach((seat) => {
     if (!seatRows[seat.row]) seatRows[seat.row] = [];
     seatRows[seat.row].push(seat);
+    allColumns.add(seat.column);
   });
+
+  const minColumn = Math.min(...Array.from(allColumns));
+  const maxColumn = Math.max(...Array.from(allColumns));
 
   return (
     <div className="flex flex-col gap-1">
       {Object.entries(seatRows)
-        .sort(([a], [b]) => a.localeCompare(b)) // row 정렬: A, B, C...
+        .sort(([a], [b]) => a.localeCompare(b)) // row 정렬
         .map(([row, seats]) => {
-          // column 정렬
-          seats.sort((a, b) => a.column - b.column);
-
-          const min = seats[0].column;
-          const max = seats[seats.length - 1].column;
-
-          const filledRow: (Seat | null)[] = Array(max - min + 1).fill(null);
-
-          seats.forEach((seat) => {
-            const index = seat.column - min;
-            filledRow[index] = seat;
-          });
-
           return (
             <SeatRow
               key={row}
-              rowSeats={filledRow}
+              rowSeats={seats}
               onSeatClick={onSeatClick}
               focusedSeatIds={focusedSeatIds}
               selectedSeatNames={selectedSeatNames}
               focusedRef={focusedRef}
               type={type}
+              minColumn={minColumn}
+              maxColumn={maxColumn}
             />
           );
         })}
